4a5db8955888531b1a21112fb41a62ad61c7f387
unknown <tankaghimire3@gmail.com>
2024-02-27 11:23:19 +0545

    phone auth

---
+++ src/context/UserAuthContext.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQOraof0vg4eM0qcdZ8SkLQV424muMOgc",
+  authDomain: "phone-auth-bbd98.firebaseapp.com",
+  projectId: "phone-auth-bbd98",
+  storageBucket: "phone-auth-bbd98.appspot.com",
+  messagingSenderId: "141054140585",
+  appId: "1:141054140585:web:ed6d64dbefcfdcd370c9b8"
+};
+
+
+const app = initializeApp(firebaseConfig);
