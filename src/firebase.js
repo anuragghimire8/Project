9854a569f2d424652ff5b7043aef128c2d306e91
unknown <tankaghimire3@gmail.com>
2024-02-28 10:37:17 +0545

    otp added/packages not working showing error

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXZtgo9SqHhnFwKIhCbBwzHouJkdkiLzo",
-  authDomain: "phoneaauth.firebaseapp.com",
-  projectId: "phoneaauth",
-  storageBucket: "phoneaauth.appspot.com",
-  messagingSenderId: "929644888380",
-  appId: "1:929644888380:web:f4851c75d0e4d850e55e39"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 
